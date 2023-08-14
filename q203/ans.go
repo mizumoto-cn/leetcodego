debbd8908a5842941bf4556e080f3a40070ab07d
徐 瑞元 <xurb@revolvesys.co.jp>
2023-08-14 17:47:56 +0900

    another203

--- q203/ans.go
+++ q203/ans.go
@@ -12,13 +12,35 @@ var _ = new(utils.Util)
 
 type ListNode = utils.ListNode
 
-func removeElements(head *ListNode, val int) *ListNode {
+func removeElements2(head *ListNode, val int) *ListNode {
 	if head == nil {
 		return nil
 	}
 	head.Next = removeElements(head.Next, val)
 	if head.Val == val {
 		return head.Next
+	}
+	return head
+}
+
+// 2ms 4.7mb
+func removeElements(head *ListNode, val int) *ListNode {
+	if head == nil {
+		return nil
+	}
+	for head != nil && head.Val == val {
+		head = head.Next
+	}
+	if head == nil {
+		return nil
+	}
+	prev := head
+	for prev.Next != nil {
+		if prev.Next.Val == val {
+			prev.Next = prev.Next.Next
+		} else {
+			prev = prev.Next
+		}
 	}
 	return head
 }
